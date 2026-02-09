2a1eb4e5a7a0c5d37f935789b1f484ccff530d07
mawutorsamuel01241788D <mawutorsamuel3@gmail.com>
2026-02-09 09:17:09 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,31 +2,12 @@
 
 using namespace std;
 
-int main(){
-    const int secretTarget = 42 // The secret number to guess
-    int guess = 0;              // user's guess
-    int attempts = 0;           // number of tries
-
-    cout << "welcome to the number guessing game!" << endl;
-    while (guess != secretTarget) {
-            // ask for user input
-            cout << "enter your guess: ";
-    cin >> guess;
-
-    //increament the attempt counter
-    attempts++;
-
-    // provid hints
-    if (guess > secretTarget){
-            cout << "too high!" << endl;
-    } else if (guess < secretTarget) {
-        cout << "too low!" << endl;
-    }
+int sum(int a, int b) {
+    return a+b;
 }
-
-    // game ended - correct guess
-    cout << "congratulation! you guessedn the number in " <<attempts << " attempts." << endl;
-
-
+int main(){
+    int result = sum(3,4);
+    cout << result << endl;
     return 0;
 }
+
